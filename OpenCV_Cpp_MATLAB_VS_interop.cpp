d717ec0c3f4eb7e4f1f46181456fda2fd9699b8c
Joshua Holloway <jhollow6@asu.edu>
2018-01-21 12:16:52 -0700

    Modified for Win7 and MATLAB r2017b

--- OpenCV_Cpp_MATLAB_VS_interop.cpp
+++ OpenCV_Cpp_MATLAB_VS_interop.cpp
@@ -24,25 +24,25 @@ int main()
 {
 	Engine *ep;	// Pointer to an MATLAB Engine
 
-	/*
-	* Start the MATLAB engine
-	*	- MATLAB runs in the background
-	*	- Access to MATLAB command prompt
-	*	- Visibility to workspace variables
-	*/
+							/*
+							* Start the MATLAB engine
+							*	- MATLAB runs in the background
+							*	- Access to MATLAB command prompt
+							*	- Visibility to workspace variables
+							*/
 	ep = engOpen(NULL);
 
 	// Reset MATLAB Environment
 	engEvalString(ep, "clc, clear, close all;");
 
 	// Evaluate string in MATLAB environment to point to a specific folder - there is nothing in this directory currently
-	engEvalString(ep, "cd C:/Users/summer2016/Desktop/Cpp_to_MATLAB/matlab_engine/matlab_dir");					// Change to active directory 
+	engEvalString(ep, "cd C:/Users/josh/Dropbox/joshMATLAB");					// Change to active directory 
 
-	// Read and display image:																																																																	
-	cv::Mat img = cv::imread("image1.png", CV_LOAD_IMAGE_GRAYSCALE);
+																																																			// Read and display image:																																																																	
+	cv::Mat img = cv::imread("image.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 	cv::imshow("OpenCV", img);
 
-	// Convert the Mat object into a double array
+	//// Convert the Mat object into a double array
 	const size_t M = img.rows;
 	const size_t N = img.cols;
 	double* arr_out = (double*)malloc(sizeof(double) * M * N);
@@ -56,8 +56,8 @@ int main()
 	engEvalString(ep, "figure, imshow(img_from_OpenCV, [],'Border','tight');");
 	engEvalString(ep, "[img_in_MATLAB, PSNR] = TestBench_moving_stats(img_from_OpenCV);");  // Call entire MATLAB scripts/functions and pass arguments
 
-	/// Retrieve data from MATLAB to process in C code
-	//	Data marshalling
+																																													/// Retrieve data from MATLAB to process in C code
+																																													//	Data marshalling
 	mxArray *PSNR;
 	//...
 
@@ -76,7 +76,7 @@ int main()
 	std::cout << "PSNR = " << *I_psnr << std::endl;
 
 	/// Close program and exit open windows
-	cv::waitKey(0); // Wait on key-press from user
+	//cv::waitKey(0); // Wait on key-press from user
 	engEvalString(ep, "close all;");
 	// De-Allocate memory
 	//...
