cdc7d34b4af9ecd37b80f43a6908e707866ce42d
Joshua Holloway <jhollow6@asu.edu>
2018-01-21 15:01:23 -0700

    Works with images and audio passing between MATLAB and C++.

--- OpenCV_Cpp_MATLAB_VS_interop.cpp
+++ OpenCV_Cpp_MATLAB_VS_interop.cpp
@@ -1,17 +1,12 @@
+// Josh Holloway
 // This is a program linking together OpenCV and MATLAB
-// Please see the program ML_Engine_Demo for the full program.
-// This version is minimal for only passing data into matlab
-// from an OpenCV Mat object
 //=============================================================================
 #include <opencv2\core\core.hpp>
 #include <opencv2\highgui\highgui.hpp>
 #include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 #include "engine.h"  // MATLAB Engine Header File required for building in Visual Studio 
 #include "mex.h"
-
-#define BUFSIZE 256
 //=============================================================================
 void linearize(const cv::Mat& mat_in, double* arr_out, const size_t M, const size_t N)
 { // Swap from row-major to col-major
@@ -20,65 +15,105 @@ void linearize(const cv::Mat& mat_in, double* arr_out, const size_t M, const siz
 			arr_out[col * M + row] = static_cast<double>(mat_in.at<unsigned char>(row, col));
 }
 //=============================================================================
+class matlabClass
+{
+private:
+	Engine *ep;				// Pointer to a MATLAB Engine
+	mxArray *mx_Arr;  // To store the image data inside MATLAB
+public:
+	matlabClass() // Default constructor
+	{
+		// Start the MATLAB engine
+		ep = engOpen(NULL);
+
+		// Reset MATLAB Environment
+		engEvalString(ep, "clc, clear, close all;");
+	}
+	~matlabClass() // Destructor
+	{
+		//free(ep);
+		engEvalString(ep, "close all;");
+	}
+	void command(const char* str)
+	{
+		engEvalString(ep, str);
+	}
+	void passImageIntoMatlab(const cv::Mat& img)
+	{
+		// Convert the Mat object into a double array
+		double* linImgArrDouble = (double*)malloc(sizeof(double) * img.rows * img.cols);
+		linearize(img, linImgArrDouble, img.rows, img.cols);
+
+		// Copy image data into an mxArray inside C++ environment
+		mx_Arr = mxCreateDoubleMatrix(img.rows, img.cols, mxREAL);
+		memcpy(mxGetPr(mx_Arr), linImgArrDouble, img.rows * img.cols * sizeof(double));
+
+		/// C++ -> MATLAB
+		// Put variable into MATLAB workstpace
+		engPutVariable(ep, "img_from_OpenCV", mx_Arr);
+		engEvalString(ep, "figure, imshow(img_from_OpenCV, [],'Border','tight');");
+	}
+	void getAudioFromMatlab()
+	{
+		// Read in audio file and play sound:
+		engEvalString(ep, "[y,Fs] = audioread('handel.wav');");
+		engEvalString(ep, "sound(y,Fs);");
+	}
+	void returnScalarFromMatlab()
+	{
+		command("matlabVal=42;");
+
+		// Grab value from workspace
+		mxArray *cppValmxArray;
+		cppValmxArray = engGetVariable(ep, "matlabVal");															// Pointer to MATLAB variable 
+		const double* cppValDblPtr = static_cast<double*>(mxGetData(cppValmxArray));	// Pointer to C variable
+		std::cout << "Value passed from MATLAB into C++ = " << *cppValDblPtr << std::endl;
+	}
+	void returnVectorFromMatlab()
+	{
+		// Create a row-vector in MATLAB
+		command("matlabVal=[1,2]");
+
+		mxArray *cppValmxArray;
+		cppValmxArray = engGetVariable(ep, "matlabVal");															// Pointer to MATLAB variable 
+		const double* cppValDblPtr = static_cast<double*>(mxGetData(cppValmxArray));	// Pointer to C variable
+		std::cout << "Vector passed from MATLAB into C++ = " << cppValDblPtr[0] << " " << cppValDblPtr[1] << std::endl;
+	}
+	//void returnMatrixFromMatlab()
+};
+//=============================================================================
 int main()
 {
-	Engine *ep;	// Pointer to an MATLAB Engine
+	// Instiantial matlab engine object
+	matlabClass matlabObj;
 
-							/*
-							* Start the MATLAB engine
-							*	- MATLAB runs in the background
-							*	- Access to MATLAB command prompt
-							*	- Visibility to workspace variables
-							*/
-	ep = engOpen(NULL);
+	// Change to active directory 
+	matlabObj.command("cd C:/Users/josh/Dropbox/joshMATLAB");
+	// Read and display image:																																																																
+	// Clear command window
+	matlabObj.command("clc, clear, close all;");
 
-	// Reset MATLAB Environment
-	engEvalString(ep, "clc, clear, close all;");
-
-	// Evaluate string in MATLAB environment to point to a specific folder - there is nothing in this directory currently
-	engEvalString(ep, "cd C:/Users/josh/Dropbox/joshMATLAB");					// Change to active directory 
-
-																																																			// Read and display image:																																																																	
+	// Read image into environment as a cv::Mat object
 	cv::Mat img = cv::imread("image.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 	cv::imshow("OpenCV", img);
 
-	//// Convert the Mat object into a double array
-	const size_t M = img.rows;
-	const size_t N = img.cols;
-	double* arr_out = (double*)malloc(sizeof(double) * M * N);
-	linearize(img, arr_out, M, N);
+	/// C++ -> MATLAB
+	// Send image data to MATLAB workspace
+	matlabObj.passImageIntoMatlab(img);
 
-	// Pass image into MATLAB and evaluate it inside a .m script
-	mxArray *mx_Arr;
-	mx_Arr = mxCreateDoubleMatrix(M, N, mxREAL);
-	memcpy(mxGetPr(mx_Arr), arr_out, M * N * sizeof(double));
-	engPutVariable(ep, "img_from_OpenCV", mx_Arr);
-	engEvalString(ep, "figure, imshow(img_from_OpenCV, [],'Border','tight');");
-	engEvalString(ep, "[img_in_MATLAB, PSNR] = TestBench_moving_stats(img_from_OpenCV);");  // Call entire MATLAB scripts/functions and pass arguments
+	/// MATLAB -> C++
+	// Read audio data from MATLAB into C++ workspace
+	matlabObj.getAudioFromMatlab();
 
-																																													/// Retrieve data from MATLAB to process in C code
-																																													//	Data marshalling
-	mxArray *PSNR;
-	//...
+	/// MATLAB -> C++
+	// Demonstrate how to pass a value from MATLAB into C++ workspace
+	matlabObj.returnScalarFromMatlab();
 
-	/// Operate on data retrieved from MATLAB
-	//...
-
-	/// Push C results to MATLAB workspace for
-	//	1. Visualization
-	//	2. Analysis
-	//	3. And Testing
-	//...
-
-	// Grab PSNR from workspace
-	PSNR = engGetVariable(ep, "PSNR");															// Pointer to MATLAB variable 
-	const double* I_psnr = static_cast<double*>(mxGetData(PSNR));		// Pointer in C variable
-	std::cout << "PSNR = " << *I_psnr << std::endl;
+	/// MATLAB -> C++
+	// Demonstrate how to pass a vector from MATLAB into C++ workspace
+	matlabObj.returnVectorFromMatlab();
 
 	/// Close program and exit open windows
-	//cv::waitKey(0); // Wait on key-press from user
-	engEvalString(ep, "close all;");
-	// De-Allocate memory
-	//...
+	cv::waitKey(0); // Wait on key-press from user
 	return 0;
 }
